e11b56f7162bd9506cb8f8455977f8c43f41491f
Leonardo Guimarães <leonardofgc@gmail.com>
2025-05-28 17:28:58 -0300

    Adicionando schema de mensagem, retorne html e teste.

--- fastapi_zero/fastapi_zero/app.py
+++ fastapi_zero/fastapi_zero/app.py
@@ -1,8 +1,18 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
-app = FastAPI()
+from fastapi_zero.schemas import Message
+
+app = FastAPI(title='Curso FastAPI 2025')
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, mundo'}
+
+
+@app.get('/helloworld', response_class=HTMLResponse)
+def hw():
+    return """<b>Hello World</b>"""
--- fastapi_zero/tests/test_app.py
+++ fastapi_zero/tests/test_app.py
@@ -1,14 +1,21 @@
 from http import HTTPStatus
+
 from fastapi.testclient import TestClient
 
 from fastapi_zero.app import app
 
 
 def test_root_deve_retornar_ola_mundo():
-    #arrange
+    # arrange
     client = TestClient(app)
     # Act
     response = client.get('/')
     # Assert
     assert response.json() == {'message': 'Olá, mundo'}
     assert response.status_code == HTTPStatus.OK
+
+
+def test_helloword_deve_retornar_o_html():
+    client = TestClient(app)
+    response = client.get('/helloworld')
+    assert response.text == '<b>Hello World</b>'
